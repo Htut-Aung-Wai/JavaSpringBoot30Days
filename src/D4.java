7d7296566871c23313a2d4cfe66e40688e46526e
Htut Aung Wai <htutaungwai333@gmail.com>
2025-01-20 12:12:28 +0630

    D4_Commited

--- src/D4.java
+++ src/D4.java
@@ -1,4 +1,32 @@
+import java.util.*;
+
 public class D4 {
+
+ public static void main(String [] args) {
+     List<String> Student = new ArrayList<>();
+     Student.add("2");
+     Student.add("1");
+     Student.add("4");
+     Student.add("3");
+     Student.add("5");
+     //Collections.sort(Student);
+     System.out.println(Student);
+     Set<String> StudentSet=new HashSet<>();
+     StudentSet.add("2");
+     StudentSet.add("1");
+     StudentSet.add("4");
+     StudentSet.add("3");
+     StudentSet.add("5");
+     System.out.println(StudentSet);
+     Map<String, Integer> fruitCount = new HashMap<>();
+     fruitCount.put("Apple", 3);
+     fruitCount.put("Banana", 5);
+     fruitCount.put("Orange", 2);
+     System.out.println(fruitCount);
+
+
+
+ }
 
 
 }
