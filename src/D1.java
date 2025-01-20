2fb4f2cf806c20e1ce9543a35a5191f402b6b85b
Htut Aung Wai <htutaungwai333@gmail.com>
2025-01-20 09:00:57 +0630

    D1_commit

---
+++ src/D1.java
@@ -0,0 +1,18 @@
+public class D1 {
+
+    String stringType= " hello";
+    int intType=12345;
+    double doubleType=4.88;
+    float floatType=5.5f;
+    public static boolean booleanType=false;
+    char charType='D';
+
+    public static void main(String [] args) {
+
+        if (booleanType) {
+            System.out.println("Boolean Condition is working");
+
+
+        }
+    }
+}
